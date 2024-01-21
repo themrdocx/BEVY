b6fee636ca5b4bf6b287458291157699b972134d
mln-grg <milankgeorge@gmail.com>
2024-01-21 18:41:35 +0530

    Added moving platforms

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovingPlatformController : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+    public float speed = 2f;
+    
+    private int currentWaypointIndex = 0;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        if (waypoints.Count > 0)
+            SetNextWaypoint();
+        else
+            Debug.LogError("No waypoints assigned to the MovingPlatformController.");
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].position, speed * Time.deltaTime);
+        
+        if (transform.position == waypoints[currentWaypointIndex].position)
+        {
+            SetNextWaypoint();
+        }
+        
+    }
+
+    private void SetNextWaypoint()
+    {
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Count;
+    }
+    
+    
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        
+        player.gameObject.transform.SetParent(transform, true);
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        
+        player.gameObject.transform.SetParent(null);
+    }
+}
