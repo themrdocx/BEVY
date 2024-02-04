9e1e9e0a5eacd2735b1ad774e9da4e98c8643ba1
MrDocx <67917129+themrdocx@users.noreply.github.com>
2024-02-04 15:44:26 +0530

    mainmenu4

---
+++ Assets/Scripts/ChangeCameraPos.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraPos : MonoBehaviour
+{
+    public Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangePos()
+    {
+        Camera.main.transform.position = pos;
+    }
+}
